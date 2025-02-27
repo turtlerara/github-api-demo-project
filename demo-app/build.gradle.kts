d67f1c57a0a8082db94a1f9c70971cfcb8b98482
turtlerara <wmg129@gmail.com>
2025-02-26 20:52:24 -0600

    adding initial spring boot app project

--- demo-app/build.gradle.kts
+++ demo-app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("project.java-app-conventions")
-    alias(libs.plugins.springBoot) apply false
+    alias(libs.plugins.springBoot)
 }
 
 apply(plugin = "io.spring.dependency-management")
@@ -9,7 +9,19 @@ group = "com.demo"
 description = "Demo App"
 version = "1.0"
 
+springBoot {
+    buildInfo()
+}
+
 dependencies {
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // Springdoc
+    implementation(libs.springdoc)
+
     // Testing
     testImplementation(libs.spock.spring)
 }
